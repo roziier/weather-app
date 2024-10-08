cb75a4ca7982d6d567d9c2c4265f7c8774fe438b
roziier <71633856+roziier@users.noreply.github.com>
2024-10-08 09:38:48 +0100

    input css

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import {BiSearch, BiCurrentLocation} from 'react-icons/bi'
 
 export default function Input() {
   return (
     <div className='p-5 flex flex-wrap justify-center items-center '>
       <div className="min-[0px]:w-full md:w-2/4 lg:w-2/4">
-        <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500" placeholder="Search City..."/>
+        <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500" placeholder="Search City..."/>
       </div>
       <div className="min-[0px]:w-full md:w-2/4 lg:w-2/4 flex justify-between items-center p-2">
-        <h2>HELLO</h2>
-        <h2>HELLO</h2>
+        <div className='flex flex-row justify-start items-center'>
+          <BiSearch size={20} className='cursor-pointer transition ease-out hover:scale-125 mr-3' />
+          <BiCurrentLocation size={20} className='cursor-pointer transition ease-out hover:scale-125' />
+        </div>
+        <div className='flex flex-row justify-start items-center'>
+          <button className='cursor-pointer transition ease-out hover:scale-125 mr-3'>°C</button>
+          |
+          <button className='cursor-pointer transition ease-out hover:scale-125 ml-3'>°F</button>
+
+        </div>
       </div>
     </div>
   )
