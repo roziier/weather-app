d0d76d663ec0d6c30681f52dbe4e25330b26aa67
roziier <71633856+roziier@users.noreply.github.com>
2024-10-08 12:26:49 +0100

    implementing daily forecast

--- src/components/DailyForecast.jsx
+++ src/components/DailyForecast.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 
-export default function DailyForecast({title, forecast}) {
+export default function DailyForecast({title}) {
   return (
     <>
         <h1 className='my-6'>{title}</h1>
         <hr className='h-px my-3 border-1 border-black w-full'/>
 
         <div className='w-full min-[0px]:flex-wrap md:flex-nowrap flex justify-center items-center overflow-visible'>
-            {forecast.map((f, index) => 
-                <div key={index} className='py-5 min-[0px]:w-full md:w-36 flex min-[0px]:flex-row md:flex-col min-[0px]:justify-between md:justify-center items-center mx-4'>
-                    <h1 className='min-[0px]:text-xs md:text-small lg:text-base lg:font-base'>{f.title}</h1>
-                    <img className='size-18' src={f.icon} alt="img" />
-                    <h1>{f.temp.toFixed(0)}</h1>
+            
+                <div className='py-5 min-[0px]:w-full md:w-36 flex min-[0px]:flex-row md:flex-col min-[0px]:justify-between md:justify-center items-center mx-4'>
+                    <h1 className='min-[0px]:text-xs md:text-small lg:text-base lg:font-base'>HELLO</h1>
+                    <h1>FFF</h1>
                 </div>
-            )}
+            
         </div>
     </>
   )
--- src/components/ForecastSection.jsx
+++ src/components/ForecastSection.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import HourlyForecast from './HourlyForecast.jsx'
+import DailyForecast from './DailyForecast.jsx'
 
 export default function ForecastSection({weather}) {
   return (
     <section className='min-[0px]:px-5 px-16 w-full flex flex-wrap flex-col justify-center items-start'>
         <HourlyForecast forecast={weather.hourly} title="3 HOURS FORECAST" />
-        
+        <DailyForecast title="5 DAYS FORECAST"/>
     </section>
   )
 }
